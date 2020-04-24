d3986369139aa7f1e748c4e25d9226bd9ef7ae9e
Aaditya Muley <aadityamuley8190@gmail.com>
2020-04-25 01:59:25 +0530

    Design changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,47 +9,50 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple[900],
+        backgroundColor: Colors.green[400],
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 60.0,
-                //width: 600.0,
-                //margin: EdgeInsets.only(left: 30.0, top: 10.0, right: 30.0),
-                padding: EdgeInsets.all(19.5),
-                color: Colors.purpleAccent[100],
-                child: Center(
-                  child: Text('Aaditya Muley'),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('images/aaditya.jpg'),
                 ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 60.0,
-                //width: 600.00,
-                //margin: EdgeInsets.only(left: 30.0, right: 30.0),
-                padding: EdgeInsets.all(7),
-                color: Colors.purpleAccent[400],
-                child: Text('IEEE Treasurer, APSIT Branch, Bombay Section'),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                height: 60.0,
-                //width: 600.00,
-                //margin: EdgeInsets.only(left: 30.0, right: 30.0),
-                padding: EdgeInsets.all(7),
-                color: Colors.purpleAccent[700],
-                child: Center(
-                  child: Text('Musician'),
+                Text(
+                  'Aaditya Muley',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 43.0,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+                Text(
+                  'IEEE Treasurer | Flutter',
+                  style: TextStyle(
+                    fontFamily: 'SpecialElite',
+                    fontSize: 17,
+                    color: Colors.green[100],
+                  ),
+                ),
+                Text(
+                  'Cloud Computing | Blockchain',
+                  style: TextStyle(
+                    fontFamily: 'SpecialElite',
+                    fontSize: 17,
+                    color: Colors.green[100],
+                  ),
+                ),
+                Text(
+                  '-------------------',
+                  style: TextStyle(
+                    fontFamily: 'SpecialElite',
+                    fontSize: 17,
+                    color: Colors.green[100],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
