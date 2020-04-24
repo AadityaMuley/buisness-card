a5f37f095f41f77642fd70e415b3bb976e576f74
Aaditya Muley <aadityamuley8190@gmail.com>
2020-04-24 23:20:53 +0530

    Changed layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,45 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.purple[900],
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0, top: 10.0),
-            padding: EdgeInsets.all(19.5),
-            color: Colors.purpleAccent,
-            child: Text('Aaditya Muley'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 60.0,
+                //width: 600.0,
+                //margin: EdgeInsets.only(left: 30.0, top: 10.0, right: 30.0),
+                padding: EdgeInsets.all(19.5),
+                color: Colors.purpleAccent[100],
+                child: Center(
+                  child: Text('Aaditya Muley'),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 60.0,
+                //width: 600.00,
+                //margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                padding: EdgeInsets.all(7),
+                color: Colors.purpleAccent[400],
+                child: Text('IEEE Treasurer, APSIT Branch, Bombay Section'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                height: 60.0,
+                //width: 600.00,
+                //margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                padding: EdgeInsets.all(7),
+                color: Colors.purpleAccent[700],
+                child: Center(
+                  child: Text('Musician'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
