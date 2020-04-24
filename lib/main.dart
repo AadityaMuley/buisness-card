f29530e6edd6c630e07790af7a95293fee508d5e
Aaditya Muley <aadityamuley8190@gmail.com>
2020-04-25 02:57:22 +0530

    Styles added

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class App extends StatelessWidget {
                   radius: 80,
                   backgroundImage: AssetImage('images/aaditya.jpg'),
                 ),
+                SizedBox(
+                  height: 30.0,
+                ),
                 Text(
                   'Aaditya Muley',
                   style: TextStyle(
@@ -26,6 +29,9 @@ class App extends StatelessWidget {
                     fontSize: 43.0,
                     color: Colors.white,
                   ),
+                ),
+                SizedBox(
+                  height: 15.0,
                 ),
                 Text(
                   'IEEE Treasurer | Flutter',
@@ -43,6 +49,9 @@ class App extends StatelessWidget {
                     color: Colors.green[100],
                   ),
                 ),
+                SizedBox(
+                  height: 10.0,
+                ),
                 Text(
                   '-------------------',
                   style: TextStyle(
@@ -51,6 +60,90 @@ class App extends StatelessWidget {
                     color: Colors.green[100],
                   ),
                 ),
+                Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.red[900],
+                        size: 50,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+91-9987538209',
+                        style: TextStyle(
+                          color: Colors.red[900],
+                          fontFamily: 'SpecialElite',
+                          fontSize: 19.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(7.0),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.red[900],
+                        size: 50,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'aadityamuley8190@ieee.com',
+                        style: TextStyle(
+                          color: Colors.red[900],
+                          fontFamily: 'SpecialElite',
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                /*Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.red[900],
+                      size: 50,
+                    ),
+                    title: Text(
+                      'aadityamuley8190@ieee.com',
+                      style: TextStyle(
+                        color: Colors.red[900],
+                        fontFamily: 'SpecialElite',
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ),*/ //Optional : Card with ListTile
               ],
             ),
           ),
